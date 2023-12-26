c9ac9f3c8e85db8f0ffc45ebcd431f91a8c89efa
HKyatham <hiteshskyatham@gmail.com>
2023-12-25 21:05:02 -0500

    Adding Mobile Manipulaor and control packages.

---
+++ robot_control/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'robot_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hitesh',
+    maintainer_email='hitesh@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'robot_teleop = robot_control.robot_teleop:main',
+            'try_inverse = robot_control.try_inverse:main',
+        ],
+    },
+)
